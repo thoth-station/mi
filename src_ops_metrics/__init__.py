32ad180ce152c99cd1dd83cd71080484dc3b71d6
Dominik Tuchyna <xtuchyna@localhost.localdomain>
2020-01-15 16:10:35 +0100

    Remove unnecessary imports in init file

--- src_ops_metrics/__init__.py
+++ src_ops_metrics/__init__.py
@@ -2,10 +2,3 @@
 
 __title__ = "src-ops-metrics"
 __version__ = "0.0.1"
-
-from .utils import *
-from .exceptions import *
-from .create_bot_knowledge import *
-from .pre_processing import *
-from .visualization import *
-from .reviewer_recommender import *
