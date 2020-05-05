ce68c82ab5b2becb2e2de86ef783bafb4098f97b
Dominik Tuchyna <dtuchyna@redhat.com>
2020-05-05 21:46:25 +0200

    correct typo

--- srcopsmetrics/storage.py
+++ srcopsmetrics/storage.py
@@ -145,7 +145,7 @@ class KnowledgeStorage:
         """Load previously collected repo knowledge. If a repo was not inspected before, create its directory.
 
         Arguments:
-            file_ath {Optional[Path]} -- path to where was stored previous knowledge from
+            file_path {Optional[Path]} -- path to where was stored previous knowledge from
                                inspected github repository
 
         Returns:
