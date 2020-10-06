0376e7b91c01d4da89752c7d8302dc6f6e3f774d
Dominik Tuchyna <dtuchyna@redhat.com>
2020-10-06 17:02:48 +0200

    Fix types & docstrings in template

--- srcopsmetrics/entities/template.py
+++ srcopsmetrics/entities/template.py
@@ -1,35 +1,38 @@
 # Copyright (C) 2020 Dominik Tuchyna
-# 
+#
 # This file is part of thoth-station/mi - Meta-information Indicators.
-# 
+#
 # thoth-station/mi - Meta-information Indicators is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # thoth-station/mi - Meta-information Indicators is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with thoth-station/mi - Meta-information Indicators.  If not, see <http://www.gnu.org/licenses/>.
 
 """Template entity class."""
 
-from typing import Any
+from typing import Any, List
+
 from voluptuous.schema_builder import Schema
+
 from srcopsmetrics.entities import Entity
 
 
 class TemplateEntity(Entity):
     """Template entity.
-    
+
     Serves as a skelet for implementing a new entity so the contributor
     does not have to spend time copying everything from interface class.
-    
+
     For further inspiration look at other implemented entities like Issue
-    or PullRequest."""
+    or PullRequest.
+    """
 
     entity_schema = Schema({"extracted_information": Any})
 
@@ -38,9 +41,8 @@ class TemplateEntity(Entity):
         self.stored = {}
         self.repository = repository
 
-    def analyse(self) -> List[GithubContentFile]:
+    def analyse(self) -> List[Any]:
         """Override :func:`~Entity.analyse`."""
-       
 
     def store(self, github_entity):
         """Override :func:`~Entity.store`."""
