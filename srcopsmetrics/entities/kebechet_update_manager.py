e4c860013de6a76fefcd75bad39c40872b5dc5d7
Dominik Tuchyna <dtuchyna@redhat.com>
2020-11-10 11:58:44 +0100

    Fix/kebechet update entity (#280)
    * Fix knowledge storing, change schema
    
    * Add fixed version with changed name
    
    * Apply pre-commit
    
    * Use get_request_type instead of is_update
    
    * Add type hint

--- srcopsmetrics/entities/kebechet_update_manager.py
+++ srcopsmetrics/entities/kebechet_update_manager.py
@@ -18,11 +18,13 @@
 """ThothYaml entity class."""
 
 import logging
-from typing import List, Optional
+import typing
 
 from github.Issue import Issue
 from github.PaginatedList import PaginatedList
 from voluptuous.schema_builder import Schema
+
+from voluptuous import Any
 
 from srcopsmetrics.entities import Entity
 
@@ -48,64 +50,57 @@ class KebechetUpdateManager(Entity):
         {
             "request_type": str,  # manual, automatic, failed
             "request_created": int,
-            "bot_first_response": int,
-            "bot_last_response": int,
-            "request_closed:": int,  # timestamp
-            "request_state": str,
+            "bot_first_response": Any(None, int),
+            "closed_at": int,
+            "merged_at": Any(None, int),
         }
     )
 
     def analyse(self) -> PaginatedList:
         """Override :func:`~Entity.analyse`."""
-        return self.get_only_new_entities()
+        return [i for i in self.get_raw_github_data() if i.number not in self.previous_knowledge]
 
     def store(self, update_request: Issue):
         """Override :func:`~Entity.store`."""
         _LOGGER.info("ID: %s", update_request.number)
-        responses = self.__class__.get_bot_responses(update_request)
 
-        self.stored_entities[update_request.number] = {
+        self.stored_entities[str(update_request.number)] = {
             "request_type": self.__class__.get_request_type(update_request),
-            "request_created": update_request.created_at.timestamp(),
-            "bot_first_response": responses[0],
-            "bot_last_response": responses[-1],
-            "request_closed:": update_request.closed_at.timestamp(),
-            "request_state": update_request.state,
+            "request_created": int(update_request.created_at.timestamp()),
+            "bot_first_response": self.__class__.get_first_bot_response(update_request),
+            "closed_at": int(update_request.closed_at.timestamp()),
+            "merged_at": self.__class__.get_merged_at(update_request),
         }
+
+    @staticmethod
+    def get_merged_at(update_request: Issue) -> typing.Optional[int]:
+        """Get merged_at time if the issue is pull_request."""
+        if update_request.pull_request:
+            if update_request.as_pull_request().merged_at:
+                return int(update_request.as_pull_request().merged_at.timestamp())
+        return None
 
     def get_raw_github_data(self):
         """Override :func:`~Entity.get_raw_github_data`."""
-        return [
-            issue for issue in self.repository.get_issues(state="closed") if self.__class__.is_update_request(issue)
-        ]
+        return [issue for issue in self.repository.get_issues(state="closed") if self.__class__.get_request_type(issue)]
 
     @staticmethod
-    def is_update_request(issue: Issue) -> bool:
-        """Find out if issue is a form of update request or not."""
-        if not issue.pull_request:
-            return issue.title == "Kebechet update"
-
-        for keyword in UPDATE_TYPES_AND_KEYWORDS.values():
-            if keyword in issue.title:
-                return True
-
-        return False
-
-    @staticmethod
-    def get_request_type(issue: Issue) -> str:
+    def get_request_type(issue: Issue) -> typing.Optional[str]:
         """Get the type of the update request."""
+        if not issue.pull_request and issue.title == "Kebechet update":
+            return "manual"
+
         for request_type, keyword in UPDATE_TYPES_AND_KEYWORDS.items():
             if keyword in issue.title:
                 return request_type
 
-        _LOGGER.info(f"Update request not recognized, issue num.{issue.number}")
-        return "not_recognized"
+        _LOGGER.debug(f"Update request not recognized, issue num.{issue.number}")
+        return None
 
     @staticmethod
-    def get_bot_responses(issue: Issue) -> List[Optional[int]]:
+    def get_first_bot_response(issue: Issue) -> typing.Optional[int]:
         """Get timestamps for all bot comments in issue."""
-        responses = []
         for comment in issue.get_comments():
             if comment.user.login in BOTS:
-                responses.append(comment.created_at.timestamp())
-        return responses if len(responses) != 0 else [None]
+                return int(comment.created_at.timestamp())
+        return None
