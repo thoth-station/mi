a2d3b2a19dfe92660684258c3cd4bd4ab2e6db68
Dominik Tuchyna <dtuchyna@redhat.com>
2021-01-27 23:08:53 +0100

    Add ttm metris

--- srcopsmetrics/metrics.py
+++ srcopsmetrics/metrics.py
@@ -18,6 +18,7 @@
 
 """Metrics for MI."""
 
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from github import Github
@@ -29,6 +30,7 @@ from srcopsmetrics.entities.pull_request import (
 from srcopsmetrics.storage import KnowledgeStorage
 
 import os
+import matplotlib.pyplot as plt
 
 _GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
 
@@ -43,7 +45,7 @@ class Metrics:
         self.prs = PullRequest(gh_repo).load_previous_knowledge(is_local=True)
         self.issues = Issue(gh_repo).load_previous_knowledge(is_local=True)
 
-    def get_aggregate_pull_requests(self):
+    def get_aggregated_pull_requests(self):
         # cols: AUTHOR PR_SIZE PR_LABELS PR_MERGE_TIME REVIEWERS TTFR
         data = []
         overall_ttfr = []
@@ -62,26 +64,64 @@ class Metrics:
             review_times = [ int(pr["reviews"][r]["submitted_at"]) for r in pr["reviews"] ]
             ttfr = min( review_times ) - created_at if review_times else None
 
+            reviews = [ r for r in pr["reviews"].values() ]
+            approvals = [ r["submitted_at"] for r in reviews if r["state"] == "APPROVED" ]
+            tta = min( approvals ) - created_at if approvals else None
+
             overall_ttfr.append( ttfr )
             overall_ttm.append( ttm )
 
             labels = [k for k in pr["labels"].keys()]
 
-            data.append( [pr["created_by"], pr["size"], labels, ttm, reviewers, ttfr] )
+            data.append( [ created_at, pr["created_by"], pr["size"], labels, reviewers, ttm, ttfr, tta] )
 
         aggregated = pd.DataFrame(data)
-        aggregated.columns = [ 'author', 'size', 'labels', 'ttm', 'reviewers', 'ttfr' ]
+        aggregated.columns = [ 'date', 'author', 'size', 'labels', 'reviewers', 'ttm', 'ttfr', 'tta' ]
 
-        y = df['rando']
-        removed_outliers = y.between(y.quantile(.05), y.quantile(.95))
-        return aggregated
+        factor = aggregated['ttm']
+        normal = factor.between( factor.quantile(.05), factor.quantile(.95) )
+        return aggregated[normal].sort_values(by=['date']).reset_index(drop=True)
 
 
-    def get_aggregate_issues(self):
+    def get_aggregated_issues(self):
         # cols: LABELS AUTHOR CLOSE_TIME
+        pass
+
+
+    def get_metrics_for_prs(self):
+        metrics = self.get_aggregated_pull_requests()[ ['date', 'ttm', 'tta', 'ttfr' ] ].copy()
+
+        metrics[ 'mttm_time'] = None
+        metrics[ 'mtta_time' ] = None
+        metrics[ 'mttfr_time' ] = None
+
+        for idx in metrics.index:
+            metrics.loc[idx, 'mttm_time'] = np.median( metrics['ttm'][:idx+1] )
+            metrics.loc[idx, 'mtta_time'] = np.median( metrics['tta'][:idx+1] )
+            metrics.loc[idx, 'mttfr_time'] = np.median( metrics['ttfr'][:idx+1] )
+
+        ttm_score = np.poly1d( np.polyfit(metrics['date'], metrics['ttm'], 10) )
+        print( f"should be minimal: {1301294.0 - ttm_score(1592385103)}" )
+        print( metrics )
+        # pd.Series( metrics['ttm'], index=metrics['date']).plot()
+        # plt.show()
+        # ts = pd.Series(metrics['ttm'], index=pd.date_range("1/1/2000", periods=1000))
+        # ts = ts.cumsum()
+        metrics[ 'datetime' ] = metrics.apply( lambda x: datetime.fromtimestamp( x['date'] ), axis=1 )
+        trendline_pts = np.linspace( metrics.loc[0, 'date'].astype( int ), metrics.loc[ len(metrics.index)-1, 'date'].astype( int ), 100)
+        plt.plot(   metrics['datetime'], metrics['ttm'].apply(lambda x: x/3600), '.',
+                    metrics['datetime'], metrics['mttm_time'].apply(lambda x: x/3600), '--',
+                    [datetime.fromtimestamp(t) for t in trendline_pts], ttm_score( trendline_pts ) / 3600, '-')
+
+        # ts.plot()
+        plt.show()
+
+
 
 
     def get_metrics_for_issues(self):
-        pass
+        aggregated = pd.DataFrame(data) #mean time to comment
+        aggregated.columns = [ 'ttci', 'mttci_time', 'ttc', 'mttc_time' ]
+
 
 
