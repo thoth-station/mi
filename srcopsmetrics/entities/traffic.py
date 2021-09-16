a199410194c0b73095c587e17a0167a97f51c1ab
Dominik Tuchyna <dtuchyna@redhat.com>
2021-09-16 18:58:37 +0200

    Add all of the traffic data

--- srcopsmetrics/entities/traffic.py
+++ srcopsmetrics/entities/traffic.py
@@ -17,8 +17,13 @@
 
 """Traffic stats class."""
 
+from datetime import datetime
 from typing import List
 
+from github.Clones import Clones
+from github.Path import Path
+from github.Referrer import Referrer
+from github.View import View
 from voluptuous.schema_builder import Schema
 from voluptuous.validators import Any
 
@@ -26,26 +31,65 @@ from srcopsmetrics.entities import Entity
 
 
 class Traffic(Entity):
-    """Template entity.
-
-    Serves as a skelet for implementing a new entity so the contributor
-    does not have to spend time copying everything from interface class.
-
-    For further inspiration look at other implemented entities like Issue
-    or PullRequest.
-    """
+    """Traffic entity."""
 
     entity_schema = Schema({int: {str: Any(str, int)}})
 
     def analyse(self) -> List[Any]:
         """Override :func:`~Entity.analyse`."""
+        return self.get_raw_github_data()
 
     def store(self, github_entity):
         """Override :func:`~Entity.store`."""
-        self.stored_entities["key"] = {
-            "extracted_information": github_entity.attribute,
-        }
+        id = None
+        type = None
+        today = datetime.today().date()
+
+        if isinstance(github_entity, View):
+            id = github_entity.timestamp
+
+            # today statistics of view and clones are never complete, have to wait for tomorrow
+            if id == today:
+                return
+
+            type = "view"
+        elif isinstance(github_entity, Clones):
+            id = github_entity.timestamp
+
+            if id == today:
+                return
+
+            type = "clones"
+        elif isinstance(github_entity, Referrer):
+            id = today
+            type = "referrer"
+        elif isinstance(github_entity, Path):
+            id = today
+            type = "path"
+
+        date_id = str(id)
+
+        if date_id not in self.previous_knowledge:
+            self.previous_knowledge[date_id] = {}
+        elif type in self.previous_knowledge[date_id]:
+            return
+
+        if date_id not in self.stored_entities:
+            self.stored_entities[date_id] = {}
+
+        data = github_entity.raw_data
+        data["type"] = type
+
+        if "timestamp" in data:
+            data.__delitem__("timestamp")
+
+        self.stored_entities[date_id] = data
 
     def get_raw_github_data(self):
         """Override :func:`~Entity.get_raw_github_data`."""
-        return self.repository.get_entity()
+        traffic_stats = self.repository.get_top_paths()
+        traffic_stats.extend(self.repository.get_top_referrers())
+        traffic_stats.extend(self.repository.get_views_traffic()["views"])
+        traffic_stats.extend(self.repository.get_clones_traffic()["clones"])
+
+        return traffic_stats
